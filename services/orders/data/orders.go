3ec96203bc5521bab341b57d4f6b6b0e9a42336f
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-24 15:09:51 +0100

    CRUD operations on orders API

---
+++ services/orders/data/orders.go
@@ -0,0 +1,116 @@
+package data
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	ctx        context.Context
+	collection *mongo.Collection
+)
+
+func init() {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalf("Error connecting to MongoDB: %s", err.Error())
+	}
+	collection = client.Database("db").Collection("orders")
+}
+
+// Order struct is a representation of a Order document
+type Order struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	SupplierID primitive.ObjectID `bson:"supplier_id" json:"supplier_id"`
+	CustomerID primitive.ObjectID `bson:"customer_id" json:"customer_id"`
+	TotalPrice float64            `bson:"total_price" json:"total_price"`
+	CreatedAt  string             `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt  string             `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+}
+
+// Orders is a slice of Order structs
+type Orders []Order
+
+// CreateOrder creates a new Order document
+func CreateOrder(order Order) (Order, int, error) {
+	order.ID = primitive.NewObjectID()
+	order.CreatedAt = time.Now().UTC().Format(time.RFC3339)
+	order.UpdatedAt = order.CreatedAt
+	_, err := collection.InsertOne(ctx, order)
+	return order, http.StatusCreated, err
+}
+
+// GetOrders returns all Orders
+func GetOrders() (Orders, int, error) {
+	var orders Orders
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return orders, http.StatusInternalServerError, err
+	}
+	if err := cursor.All(ctx, &orders); err != nil {
+		return orders, http.StatusInternalServerError, err
+	}
+	if orders == nil {
+		return Orders{}, http.StatusNotFound, nil
+	}
+	return orders, http.StatusOK, nil
+}
+
+// GetOrder returns a Order by ID
+func GetOrder(id string) (Order, int, error) {
+	var order Order
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return order, http.StatusBadRequest, err
+	}
+	err = collection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&order)
+	if err != nil {
+		return order, http.StatusNotFound, err
+	}
+	return order, http.StatusOK, nil
+}
+
+// UpdateOrder updates a Order by ID
+func UpdateOrder(id string, order Order) (Order, int, error) {
+	// check if order exists
+	_, status, err := GetOrder(id)
+	if err != nil {
+		return order, status, err
+	}
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return order, http.StatusBadRequest, err
+	}
+	order.UpdatedAt = time.Now().UTC().Format(time.RFC3339)
+	_, err = collection.UpdateOne(ctx, bson.M{"_id": objectID}, bson.M{"$set": order})
+	if err != nil {
+		return order, http.StatusInternalServerError, err
+	}
+	return order, http.StatusOK, nil
+}
+
+// DeleteOrder deletes a Order by ID
+func DeleteOrder(id string) (int, error) {
+	// check if order exists
+	_, status, err := GetOrder(id)
+	if err != nil {
+		return status, err
+	}
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	_, err = collection.DeleteOne(ctx, bson.M{"_id": objectID})
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	return http.StatusOK, nil
+}
--- services/orders/main.go
+++ services/orders/main.go
@@ -1,12 +1,75 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+
+	"github.com/Omar-Belghaouti/pdash/services/orders/data"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Respone struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Orders")
+	// Create a new Order
+	app.Post("/orders", func(c *fiber.Ctx) error {
+		order := data.Order{}
+		if err := c.BodyParser(&order); err != nil {
+			return c.Status(http.StatusBadRequest).JSON(Respone{Message: err.Error()})
+		}
+		order, status, err := data.CreateOrder(order)
+		if err != nil {
+			return c.Status(status).JSON(Respone{Message: err.Error()})
+		}
+		return c.Status(status).JSON(order)
+	})
+
+	// Get all Orders
+	app.Get("/orders", func(c *fiber.Ctx) error {
+		orders, status, err := data.GetOrders()
+		if err != nil {
+			return c.Status(status).JSON(Respone{Message: err.Error()})
+		}
+		return c.Status(status).JSON(orders)
+	})
+
+	// Get a Order by ID
+	app.Get("/orders/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		order, status, err := data.GetOrder(id)
+		if err != nil {
+			return c.Status(status).JSON(Respone{Message: err.Error()})
+		}
+		return c.Status(status).JSON(order)
+	})
+
+	// Update a Order by ID
+	app.Put("/orders/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		order := data.Order{}
+		if err := c.BodyParser(&order); err != nil {
+			return c.Status(http.StatusBadRequest).JSON(Respone{Message: err.Error()})
+		}
+		order, status, err := data.UpdateOrder(id, order)
+		if err != nil {
+			return c.Status(status).JSON(Respone{Message: err.Error()})
+		}
+		return c.Status(status).JSON(Respone{Message: "Order updated successfully"})
+	})
+
+	// Delete a Order by ID
+	app.Delete("/orders/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		status, err := data.DeleteOrder(id)
+		if err != nil {
+			return c.Status(status).JSON(Respone{Message: err.Error()})
+		}
+		return c.Status(status).JSON(Respone{
+			Message: "Order deleted",
+		})
 	})
 
 	app.Listen(":3002")
