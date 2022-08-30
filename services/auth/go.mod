3486bdf607510a6b39cd6afc4d08a69641541bfe
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-30 20:20:42 +0100

    add swagger to auth service

--- services/auth/go.mod
+++ services/auth/go.mod
@@ -3,10 +3,12 @@ module github.com/Omar-Belghaouti/pdash/services/auth
 go 1.19
 
 require (
-	github.com/gofiber/fiber/v2 v2.36.0
+	github.com/arsmn/fiber-swagger/v2 v2.31.1
+	github.com/gofiber/fiber/v2 v2.37.0
 	github.com/google/uuid v1.3.0
 	github.com/o1egl/paseto v1.0.0
 	github.com/spf13/viper v1.12.0
+	github.com/swaggo/swag v1.8.5
 	go.mongodb.org/mongo-driver v1.10.1
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
 	google.golang.org/grpc v1.46.2
@@ -14,16 +16,23 @@ require (
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
 	github.com/aead/chacha20poly1305 v0.0.0-20170617001512-233f39982aeb // indirect
 	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.7 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
@@ -34,17 +43,19 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
+	github.com/swaggo/files v0.0.0-20220728132757-551d4a08d97a // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.38.0 // indirect
+	github.com/valyala/fasthttp v1.39.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- services/auth/main.go
+++ services/auth/main.go
@@ -7,7 +7,9 @@ import (
 	"sync"
 
 	"github.com/Omar-Belghaouti/pdash/services/auth/data"
+	_ "github.com/Omar-Belghaouti/pdash/services/auth/docs"
 	"github.com/Omar-Belghaouti/pdash/services/auth/pb"
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"google.golang.org/grpc"
@@ -17,6 +19,13 @@ type Respone struct {
 	Message string `json:"message"`
 }
 
+// @title pdash auth service
+// @version 1.0
+// @description pdash auth service
+// @contact.name Omar Belghaouti
+// @contact.email omarbelghaouti@gmail.com
+// @host localhost:8004
+// @BasePath /
 func main() {
 	var wg sync.WaitGroup
 
@@ -48,34 +57,63 @@ func main() {
 			AllowMethods: "POST",
 		}))
 
+		// Swagger
+		app.Get("/swagger/*", swagger.HandlerDefault)
+
 		// Create a new user
-		app.Post("/users", func(c *fiber.Ctx) error {
-			user := data.User{}
-			if err := c.BodyParser(&user); err != nil {
-				return c.Status(http.StatusBadRequest).JSON(Respone{Message: err.Error()})
-			}
-			user, status, err := data.CreateUser(user)
-			if err != nil {
-				return c.Status(status).JSON(Respone{Message: err.Error()})
-			}
-			return c.Status(status).JSON(user)
-		})
+		app.Post("/users", CreateUser)
 
 		// Login a user
-		app.Post("/users/login", func(c *fiber.Ctx) error {
-			req := data.LoginUserRequest{}
-			if err := c.BodyParser(&req); err != nil {
-				return c.Status(http.StatusBadRequest).JSON(Respone{Message: err.Error()})
-			}
-			user, status, err := data.LoginUser(req)
-			if err != nil {
-				return c.Status(status).JSON(Respone{Message: err.Error()})
-			}
-			return c.Status(status).JSON(user)
-		})
+		app.Post("/users/login", LoginUser)
 
 		app.Listen("0.0.0.0:3004")
 	}()
 
 	wg.Wait()
+}
+
+// CreateUser creates a new user
+// @Summary Create a new user
+// @Description Create a new user
+// @ID create-user
+// @Accept  json
+// @Produce  json
+// @Param user body data.User true "User"
+// @Success 201 {object} data.User
+// @Failure 400 {object} Respone
+// @Failure 500 {object} Respone
+// @Router /users [post]
+func CreateUser(c *fiber.Ctx) error {
+	user := data.User{}
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(Respone{Message: err.Error()})
+	}
+	user, status, err := data.CreateUser(user)
+	if err != nil {
+		return c.Status(status).JSON(Respone{Message: err.Error()})
+	}
+	return c.Status(status).JSON(user)
+}
+
+// LoginUser logs in a user
+// @Summary Login a user
+// @Description Login a user
+// @ID login-user
+// @Accept  json
+// @Produce  json
+// @Param user body data.LoginUserRequest true "User"
+// @Success 200 {object} data.LoginUserResponse
+// @Failure 400 {object} Respone
+// @Failure 500 {object} Respone
+// @Router /users/login [post]
+func LoginUser(c *fiber.Ctx) error {
+	req := data.LoginUserRequest{}
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(Respone{Message: err.Error()})
+	}
+	user, status, err := data.LoginUser(req)
+	if err != nil {
+		return c.Status(status).JSON(Respone{Message: err.Error()})
+	}
+	return c.Status(status).JSON(user)
 }
