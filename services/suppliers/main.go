42d95f7e37a67ce2a125fbc2b40dedc6b66320d4
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-30 21:07:48 +0100

    add swagger to suppliers service

--- services/suppliers/main.go
+++ services/suppliers/main.go
@@ -9,7 +9,9 @@ import (
 	"sync"
 
 	"github.com/Omar-Belghaouti/pdash/services/suppliers/data"
+	_ "github.com/Omar-Belghaouti/pdash/services/suppliers/docs"
 	"github.com/Omar-Belghaouti/pdash/services/suppliers/pb"
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"google.golang.org/grpc"
@@ -23,6 +25,13 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+// @title pdash suppliers service
+// @version 1.0
+// @description pdash suppliers service
+// @contact.name Omar Belghaouti
+// @contact.email omarbelghaouti@gmail.com
+// @host localhost:8003
+// @BasePath /
 func main() {
 	var wg sync.WaitGroup
 	grpcAuthClient := make(chan pb.AuthServiceClient)
@@ -71,6 +80,9 @@ func main() {
 			AllowMethods: "GET, POST, PUT, DELETE",
 		}))
 
+		// Swagger
+		app.Get("/swagger/*", swagger.HandlerDefault)
+
 		// Auth middleware
 		app.Use(func(c *fiber.Ctx) error {
 			authClient := <-grpcAuthClient
@@ -98,63 +110,132 @@ func main() {
 		})
 
 		// Create a new Supplier
-		app.Post("/suppliers", func(c *fiber.Ctx) error {
-			supplier := data.Supplier{}
-			if err := c.BodyParser(&supplier); err != nil {
-				return c.Status(http.StatusBadRequest).JSON(Response{Message: err.Error()})
-			}
-			supplier, status, err := data.CreateSupplier(supplier)
-			if err != nil {
-				return c.Status(status).JSON(Response{Message: err.Error()})
-			}
-			return c.Status(status).JSON(supplier)
-		})
+		app.Post("/suppliers", CreateSupplier)
 
 		// Get all Suppliers
-		app.Get("/suppliers", func(c *fiber.Ctx) error {
-			suppliers, status, err := data.GetSuppliers()
-			if err != nil {
-				return c.Status(status).JSON(Response{Message: err.Error()})
-			}
-			return c.Status(status).JSON(suppliers)
-		})
+		app.Get("/suppliers", GetSuppliers)
 
 		// Get a Supplier by ID
-		app.Get("/suppliers/:id", func(c *fiber.Ctx) error {
-			id := c.Params("id")
-			supplier, status, err := data.GetSupplier(id)
-			if err != nil {
-				return c.Status(status).JSON(Response{Message: err.Error()})
-			}
-			return c.Status(status).JSON(supplier)
-		})
+		app.Get("/suppliers/:id", GetSupplierByID)
 
 		// Update a Supplier by ID
-		app.Put("/suppliers/:id", func(c *fiber.Ctx) error {
-			id := c.Params("id")
-			supplier := data.Supplier{}
-			if err := c.BodyParser(&supplier); err != nil {
-				return c.Status(http.StatusBadRequest).JSON(Response{Message: err.Error()})
-			}
-			supplier, status, err := data.UpdateSupplier(id, supplier)
-			if err != nil {
-				return c.Status(status).JSON(Response{Message: err.Error()})
-			}
-			return c.Status(status).JSON(Response{Message: "Supplier updated successfully"})
-		})
+		app.Put("/suppliers/:id", UpdateSupplierByID)
 
 		// Delete a Supplier by ID
-		app.Delete("/suppliers/:id", func(c *fiber.Ctx) error {
-			id := c.Params("id")
-			status, err := data.DeleteSupplier(id)
-			if err != nil {
-				return c.Status(status).JSON(Response{Message: err.Error()})
-			}
-			return c.Status(status).JSON(Response{Message: "Supplier deleted successfully"})
-		})
+		app.Delete("/suppliers/:id", DeleteSupplierByID)
 
 		app.Listen("0.0.0.0:3003")
 	}()
 
 	wg.Wait()
+}
+
+// CreateSupplier creates a new Supplier
+// @Summary Create a new Supplier
+// @Description Create a new Supplier
+// @ID create-supplier
+// @Accept  json
+// @Produce  json
+// @Param supplier body data.Supplier true "Supplier"
+// @Success 201 {object} data.Supplier
+// @Failure 400 {object} Response
+// @Failure 500 {object} Response
+// @Router /suppliers [post]
+func CreateSupplier(c *fiber.Ctx) error {
+	supplier := data.Supplier{}
+	if err := c.BodyParser(&supplier); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(Response{Message: err.Error()})
+	}
+	supplier, status, err := data.CreateSupplier(supplier)
+	if err != nil {
+		return c.Status(status).JSON(Response{Message: err.Error()})
+	}
+	return c.Status(status).JSON(supplier)
+}
+
+// GetSuppliers gets all Suppliers
+// @Summary Get all Suppliers
+// @Description Get all Suppliers
+// @ID get-suppliers
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} data.Supplier
+// @Failure 404 {object} Response
+// @Failure 400 {object} Response
+// @Failure 500 {object} Response
+// @Router /suppliers [get]
+func GetSuppliers(c *fiber.Ctx) error {
+	suppliers, status, err := data.GetSuppliers()
+	if err != nil {
+		return c.Status(status).JSON(Response{Message: err.Error()})
+	}
+	return c.Status(status).JSON(suppliers)
+}
+
+// GetSupplierByID gets a Supplier by ID
+// @Summary Get a Supplier by ID
+// @Description Get a Supplier by ID
+// @ID get-supplier-by-id
+// @Accept  json
+// @Produce  json
+// @Param id path string true "ID"
+// @Success 200 {object} data.Supplier
+// @Failure 404 {object} Response
+// @Failure 400 {object} Response
+// @Failure 500 {object} Response
+// @Router /suppliers/{id} [get]
+func GetSupplierByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	supplier, status, err := data.GetSupplier(id)
+	if err != nil {
+		return c.Status(status).JSON(Response{Message: err.Error()})
+	}
+	return c.Status(status).JSON(supplier)
+}
+
+// UpdateSupplierByID updates a Supplier by ID
+// @Summary Update a Supplier by ID
+// @Description Update a Supplier by ID
+// @ID update-supplier-by-id
+// @Accept  json
+// @Produce  json
+// @Param id path string true "ID"
+// @Param supplier body data.Supplier true "Supplier"
+// @Success 200 {object} Response
+// @Failure 404 {object} Response
+// @Failure 400 {object} Response
+// @Failure 500 {object} Response
+// @Router /suppliers/{id} [put]
+func UpdateSupplierByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	supplier := data.Supplier{}
+	if err := c.BodyParser(&supplier); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(Response{Message: err.Error()})
+	}
+	supplier, status, err := data.UpdateSupplier(id, supplier)
+	if err != nil {
+		return c.Status(status).JSON(Response{Message: err.Error()})
+	}
+	return c.Status(status).JSON(Response{Message: "Supplier updated successfully"})
+}
+
+// DeleteSupplierByID deletes a Supplier by ID
+// @Summary Delete a Supplier by ID
+// @Description Delete a Supplier by ID
+// @ID delete-supplier-by-id
+// @Accept  json
+// @Produce  json
+// @Param id path string true "ID"
+// @Success 200 {object} Response
+// @Failure 404 {object} Response
+// @Failure 400 {object} Response
+// @Failure 500 {object} Response
+// @Router /suppliers/{id} [delete]
+func DeleteSupplierByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	status, err := data.DeleteSupplier(id)
+	if err != nil {
+		return c.Status(status).JSON(Response{Message: err.Error()})
+	}
+	return c.Status(status).JSON(Response{Message: "Supplier deleted successfully"})
 }
